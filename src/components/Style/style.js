8831bfd46027eae17be26cf279cfcaede0012a91
uiyeon831 <104365642+uiyeon831@users.noreply.github.com>
2023-11-28 18:17:52 +0900

    apply styled component

---
+++ src/components/Style/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Logo = styled.p`
+  width: 70px;
+  font-size: 28px;
+  font-weight: 600;
+  text-align: left;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const ATag = styled.a`
+  text-decoration: none;
+`
+
+export {Logo, ATag};
