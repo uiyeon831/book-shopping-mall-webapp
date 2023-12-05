9f77c65eb886981ea5efd76bea5388c91c1535d7
uiyeon831 <104365642+uiyeon831@users.noreply.github.com>
2023-12-05 17:27:19 +0900

    change style

--- src/components/Category/style.js
+++ src/components/Category/style.js
@@ -1,25 +1,30 @@
 import styled from "styled-components";
 
 const Menu = styled.ul`
-  width: 10%;
-  height: 60vh;
-  background-color: #ebebeb;
-  border-radius: 10px;
-  min-width: 180px;
-  max-width: 180px;
-  text-align: center;
+  height: 70vh;
+  background-color: #f5f5f5;
+  min-width: 150px;
+  max-width: 150px;
   margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 30px 0;
+  position: sticky;
+  top: 20px;
 `
 
 const MenuLi = styled.li`
+  width: 85px;
   height: 20px;
   line-height: 20px;
   letter-spacing: 2px;
   font-size:  15px;
   color: #3c3c3c;
   display: list-item;
-  padding: 15px;
-  border-bottom: 0.5px solid #3c3c3c;
+  padding: 20px;
+
   &:hover{
     cursor: pointer;
     font-weight: 600;
@@ -32,9 +37,15 @@ const MenuTitle = styled.p`
   font-size: 18px;
   letter-spacing: 2px;
   padding: 10px;
-  border-bottom: 2px solid #3c3c3c;
   margin: 0px;
   font-weight: 600;
 `
 
-export {Menu, MenuLi, MenuTitle};
+const MenuLine = styled.div`
+  width: 120px;
+  height: 1px;
+  background-color: #8f8f8f;
+  margin: 20px 0;
+`
+
+export {Menu, MenuLi, MenuTitle, MenuLine};
